da96c0f1eccd6d11725877262a24ad91f9d2240b
10Foldz <anggaajik03@gmail.com>
2024-06-10 16:16:24 +0700

    change layout

--- resources/views/flow/productpage.blade.php
+++ resources/views/flow/productpage.blade.php
@@ -8,14 +8,74 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
         body {
-            background-image: url('assets/background.png');
+            background: url('/images/background2.jpg') no-repeat center center fixed;
+            background-size: cover;
             background-size: cover;
             background-repeat: no-repeat;
             background-attachment: fixed;
         }
+        .back-button {
+            position: absolute;
+            bottom: 30px;
+            left: 30px;
+        }
+        .back-button a {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 1em;
+            background-color: #333;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 15px;
+            box-shadow: 3px 3px 10px #00000070;
+            transition: background-color 0.3s ease, box-shadow 0.3s ease;
+        }
+        .back-button a:hover {
+            background-color: #555;
+            box-shadow: 3px 3px 15px #00000090;
+        }
+        .nav {
+            position: absolute;
+            top: 30px;
+            right: 30px;
+            display: flex;
+            gap: 80px;
+            align-items: center;
+        }
+        .nav a {
+            color: #fff;
+            text-decoration: none;
+            font-size: 1.5em;
+            transition: color 0.3s ease;
+        }
+        .nav a:hover {
+            color: #bbb;
+        }
+        .hamburger {
+            width: 30px;
+            height: 22px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            cursor: pointer;
+        }
+        .hamburger div {
+            width: 100%;
+            height: 4px;
+            background-color: #fff;
+        }
+        .logo {
+            position: absolute;
+            top: 30px;
+            left: 30px;
+            font-size: 1.5em;
+            z-index: 1;
+            color: #fff;
+        }
     </style>
 </head>
 <body>
+    <div class="logo">BONSAIMART</div>
     <h4 class="text-center mt-md-5 mb-md-2" style="color: white; font-size: 35px">Our Product</h4>
     <hr style="color: white;">
     <div class="content mt-5 d-flex flex-lg-wrap gap-4">
@@ -80,6 +140,19 @@
                 <button class="btn btn-outline-primary" style="font-size:15px">
                     <i class="fa-solid fa-cart-shopping"></i>
                 </button>
+            </div>
+        </div>
+        <div class="back-button">
+            <a href="{{ route('seller.view') }}">← Back</a>
+        </div>
+        <div class="nav">
+            <a href="#">About</a>
+            <a href="#">Product</a>
+            <a href="#">Contact</a>
+            <div class="hamburger">
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
         </div>
     </div>
